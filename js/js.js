11b087c52ae6bdd2b2ba7ea2e840303e8fd66d2b
sergio <sergiomancilla13@gmail.com>
2021-10-01 17:39:17 -0300

    arreglo funcion

--- js/js.js
+++ js/js.js
@@ -19,33 +19,22 @@ let titulo = d.getElementById('titulo');
 const spinner = document.getElementById('spinner');
 const mapa = d.getElementById('mapa');
 
-
-
-
 const busquedaGuardada = JSON.parse(localStorage.getItem('busqueda'));
+
 if(busquedaGuardada != null){
 
-    temp.innerHTML = busquedaGuardada.main.temp;
-    tempMin.innerHTML = busquedaGuardada.main.temp_min;
-    tempMax.innerHTML = busquedaGuardada.main.temp_max;
-    humedad.innerHTML = busquedaGuardada.main.humidity + "%";
-    sensacionT.innerHTML = busquedaGuardada.main.feels_like;
-    presionAt.innerHTML = busquedaGuardada.main.pressure;
-    velocidadV.innerHTML = busquedaGuardada.wind.speed;
-    mapa.src = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBtauoCs08lNVbY5ST4UqfjkFUjofgoXok&q=${busquedaGuardada.name}&center=${busquedaGuardada.coord.lat},${busquedaGuardada.coord.lon}`;
+    insertarDatos(busquedaGuardada);
     buscarGif(busquedaGuardada.weather[0]);
-    descripcion.innerHTML = busquedaGuardada.weather[0].description;
-    titulo.innerHTML = busquedaGuardada.name;
 
     divDatos.style.display = 'block';
     sectionMapa.style.display = 'block';
 }
+
 btnBuscar.addEventListener('click',  () => {
-    console.log("ciudad es", ciudad.value);
-    console.log("click buscar xD");
+    //console.log("ciudad es", ciudad.value);
     showSpinner();
     buscarClima(ciudad.value);
-   divDatos.style.display = 'block';
+    divDatos.style.display = 'block';
     sectionMapa.style.display = 'block';
 });
 
@@ -58,31 +47,20 @@ function buscarClima(ciudad) {
                 console.log("error miaa");
             } else {
                 guardarBusqueda(rta);
-                console.log(rta);
-                console.log(iconClima);
-                temp.innerHTML = rta.main.temp;
-                tempMin.innerHTML = rta.main.temp_min;
-                tempMax.innerHTML = rta.main.temp_max;
-                humedad.innerHTML = rta.main.humidity + "%";
-                sensacionT.innerHTML = rta.main.feels_like;
-                presionAt.innerHTML = rta.main.pressure;
-                velocidadV.innerHTML = rta.wind.speed;
+                console.log("rta clima",rta);
                 //iconClima.src = `http://openweathermap.org/img/wn/${rta.weather[0].icon}@2x.png`;
-                mapa.src = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBtauoCs08lNVbY5ST4UqfjkFUjofgoXok&q=${ciudad}&center=${rta.coord.lat},${rta.coord.lon}`;
-
+                insertarDatos(rta);
                 buscarGif(rta.weather[0]);
-                descripcion.innerHTML = rta.weather[0].description;
-                titulo.innerHTML = rta.name;
                 showSpinner();
             }
         })
         .catch(function () {
-            console.log("Algoo salio mal!");
+            console.log("Algo salio mal!");
     });
 }
 
 function buscarGif(clima) {
-           console.log(clima.main);
+           //console.log(clima.main);
            imgClima.src = "img/"+clima.main+".gif";
            imgClima.alt = clima.description;
 }
@@ -93,4 +71,17 @@ function showSpinner() {
 
 function guardarBusqueda(ciudadBuscada) {
     localStorage.setItem('busqueda', JSON.stringify(ciudadBuscada));
+}
+
+function insertarDatos(response) {
+    temp.innerHTML = response.main.temp;
+    tempMin.innerHTML = response.main.temp_min;
+    tempMax.innerHTML = response.main.temp_max;
+    humedad.innerHTML = response.main.humidity + "%";
+    sensacionT.innerHTML = response.main.feels_like;
+    presionAt.innerHTML = response.main.pressure;
+    velocidadV.innerHTML = response.wind.speed;
+    mapa.src = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBtauoCs08lNVbY5ST4UqfjkFUjofgoXok&q=${response.name}&center=${response.coord.lat},${response.coord.lon}`;
+    descripcion.innerHTML = response.weather[0].description;
+    titulo.innerHTML = response.name;
 }
